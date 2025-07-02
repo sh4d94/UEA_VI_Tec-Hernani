9053cee1f20afa55c31047b8d213c0492fcdf584
sh4d94 <junnou4d@gmail.com>
2025-07-01 19:52:56 -0600

    webcam p5 y td

---
+++ webcam_p5/comparacion.md
@@ -0,0 +1,4 @@
+# WebCam en p5 y TouchDesigner
+## Diferencias y similitudes
+
+Tanto **p5** como **TouchDesigner** permiten acceder a la _Webcam_ e invertir la imagen con un _flip_, pero lo hacen de manera diferente. En p5, todo debe hacerse manualmente en el _canvas_, por lo que fue difícil centrarla. En cambio, TD ofrece ver todo en tiempo real. Ahí tuve problemas al intentar dar salida al video. Esta diferencia me permitió notar que en TD los errores son más visibles al instante, mientras que en p5, sin un segundo monitor, es más difícil identificar lo que sucede.
--- webcam_p5/sketch.js
+++ webcam_p5/sketch.js
@@ -1,5 +1,10 @@
 let webcam;
 
+//I write the explications in the comments in english, because I am starting to learn it, and I want to practice my english skills.
+//This code uses the p5 library to create a simple sketch that displays a webcam feed
+//The webcam feed is displayed in the center of the canvas, and it is flipped horizontally to
+//simulate a mirror effect. The webcam feed is also resized to 400x400 pixels.
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   webcam = createCapture(VIDEO, {flipped: true}); // The flipped option is set to true to mirror the video, it means the video will appear as if you are looking in a mirror.
